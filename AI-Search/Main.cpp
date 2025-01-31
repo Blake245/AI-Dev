449cdf8cccfe4da70ec2f26d8d250d754cc952cd
Blake245 <jwillden@student.neumont.edu>
2025-01-31 15:35:10 -0700

    DFS

---
+++ AI-Search/Main.cpp
@@ -0,0 +1,83 @@
+#include "Node.h"
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+using c_node_t = Node<char>;
+
+vector<c_node_t*> DFS(c_node_t* root, char target)
+{
+	stack<c_node_t*> node_stack;
+	root->visited = true;
+	node_stack.push(root);
+	while (!node_stack.empty())
+	{
+		c_node_t* current_node = node_stack.top();
+		if (current_node->data == target)
+		{
+			cout << "Found: " << current_node->data << endl;
+			break;
+		}
+		else
+		{
+			cout << "Visited: " << current_node->data << endl;
+		}
+
+		bool popNode = true;
+
+		for (auto child : current_node->children)
+		{
+			if (!child->visited)
+			{
+				popNode = false;
+				child->visited = true;
+				node_stack.push(child);
+				break;
+			}
+		}
+
+		if (popNode)
+		{
+			node_stack.pop();
+		}
+	}
+
+	vector<c_node_t*> path;
+	while (!node_stack.empty())
+	{
+		path.insert(path.begin(), node_stack.top());
+		node_stack.pop();
+
+	}
+
+	return path;
+}
+
+int main()
+{
+	c_node_t* nodeA = new c_node_t('A');
+	c_node_t* nodeB = new c_node_t('B');
+	c_node_t* nodeC = new c_node_t('C');
+	c_node_t* nodeD = new c_node_t('D');
+	c_node_t* nodeE = new c_node_t('E');
+	c_node_t* nodeF = new c_node_t('F');
+
+	nodeA->children.push_back(nodeB);
+	nodeA->children.push_back(nodeC);
+
+	nodeB->children.push_back(nodeD);
+	nodeB->children.push_back(nodeE);
+
+	nodeC->children.push_back(nodeE);
+	nodeC->children.push_back(nodeF);
+
+	auto path = DFS(nodeA, 'F');
+
+	cout << "Path: ";
+	for (auto node : path)
+	{
+		cout << node->data << "->";
+	}
+	cout << endl;
+}
